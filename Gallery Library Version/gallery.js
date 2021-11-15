923e63a0a708d0d2b448b04575f48244f65419d1
Demonohu <64247245+Demonohu@users.noreply.github.com>
2021-11-15 18:52:42 +0100

    Add files via upload

---
+++ Gallery Library Version/gallery.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+
+  speed: 400,
+  spaceBetween: 100,
+  effect: 'coverflow',
+
+  // Optional parameters
+  direction: 'horizontal',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
